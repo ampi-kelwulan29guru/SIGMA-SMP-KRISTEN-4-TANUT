0b201013a82fa88098b80de39fc8eaaf26665535
ampi-kelwulan29guru <abraham.kelwulan43@guru.smp.belajar.id>
2026-07-31 01:01:56 +0700

    Delete css/js directory

--- css/js/app.js
+++
@@ -1,6 +0,0 @@
-// ======================================
-// SIGMA EDU PRO
-// Sistem Informasi Guru dan Administrasi
-// ======================================
-
-console.log("SIGMA EDU PRO berhasil dijalankan.");
