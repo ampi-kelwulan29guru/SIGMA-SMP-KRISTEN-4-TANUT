f3e2bddda7a6c846c817d575dd01cf6ddafa7211
ampi-kelwulan29guru <abraham.kelwulan43@guru.smp.belajar.id>
2026-07-31 00:50:35 +0700

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,1 +1,6 @@
+// ======================================
+// SIGMA EDU PRO
+// Sistem Informasi Guru dan Administrasi
+// ======================================
 
+console.log("SIGMA EDU PRO berhasil dijalankan.");
